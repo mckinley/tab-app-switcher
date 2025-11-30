dfed5befa1b04e9d43c12ac8965dcf44d6cd4ebe
Bronson McKinley <mckinley@users.noreply.github.com>
2025-11-30 14:27:29 -0800

    Release v1.0.3

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   manifest: {
     name: "Tab Application Switcher",
     description: "Switch between browser tabs like you switch between applications",
-    version: "1.0.2",
+    version: "1.0.3",
     permissions: ["tabs", "storage"],
     host_permissions: ["<all_urls>"],
     icons: {
