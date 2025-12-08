f01f648d287c320955957cdaabe8d854e5de43a8
Bronson McKinley <mckinley@users.noreply.github.com>
2025-12-08 11:22:26 -0800

    Release v1.1.0

--- extension/wxt.config.ts
+++ extension/wxt.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
     name: "__MSG_extName__",
     description: "__MSG_extDescription__",
     default_locale: "en",
-    version: "1.0.7",
+    version: "1.1.0",
     homepage_url: "https://tabappswitcher.com",
     author: "Tab Application Switcher",
     permissions: ["tabs", "storage", "identity"],
