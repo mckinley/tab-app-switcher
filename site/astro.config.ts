5899ce39ff529085498a2d3ceb200107d5dcc8b5
Bronson McKinley <mckinley@users.noreply.github.com>
2026-04-28 16:48:28 -0700

    feat: pin dev port via .env.development

--- site/astro.config.ts
+++ site/astro.config.ts
@@ -8,6 +8,7 @@ import { fileURLToPath } from "url"
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
+  server: { port: parseInt(process.env.PORT ?? '', 10) || undefined },
   adapter: cloudflare({
     platformProxy: { enabled: true },
   }),
